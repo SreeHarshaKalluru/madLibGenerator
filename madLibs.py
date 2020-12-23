2fba5cd16e77731f66f72eb517a96262d3826622
sreeharshak <sreeharshak@spiceworks.com>
2020-12-23 09:04:24 +0530

    Added new story(chicken fight) to the mad lib

--- madLibs.py
+++ madLibs.py
@@ -1,4 +1,4 @@
-stories = {1:"vacuum", 2:'flip_flops'}
+stories = {1:"vacuum", 2:'flip_flops', 3:'chicken fight'}
 for i in stories:
     print(i, stories[i])
 id = int(input("Enter any story id from the above : "))
@@ -10,5 +10,8 @@ if id == 1:
 elif id == 2:
     adjective = input("Enter an adjective : ")
     print("Flip flops are staple of any " + adjective + " summer wadrobe")
+elif id == 3:
+    adjective = input("Enter an adjective : ")
+    print("If you're going to challenge a couple to a chicken fight during spring break, make sure they're more " + adjective + ' than you!')
 else:
     print("Invalid story selected.")
